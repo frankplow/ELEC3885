d065ccb648a7652dcca7fd3639b8ce3cfac483b5
Frank Plowman <post@frankplowman.com>
2023-03-24 18:00:28 +0000

    Container module skeleton

---
+++ firmware/f7/Container/Inc/container.h
@@ -0,0 +1,18 @@
+///
+/// @file container.h
+/// @brief Interface for ELEC3885 container file module
+///
+/// This file is used to manage the creation and modification
+/// of a QuickTime/MP4 file.
+///
+
+#ifndef CONTAINER_H_
+#define CONTAINER_H_
+
+#include "events.h"
+
+void on_dcmi_data_ready(DCMIDataReadyEventData data);
+
+void on_dcmi_frame_complete(DCMIFrameCompleteEventData data);
+
+#endif  // CONTAINER_H_
---
+++ firmware/f7/Container/Src/container.c
@@ -0,0 +1,7 @@
+#include "container.h"
+
+void on_dcmi_data_ready(DCMIDataReadyEventData data) {
+}
+
+void on_dcmi_frame_complete(DCMIFrameCompleteEventData data) {
+}
