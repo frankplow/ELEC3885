50c4425902f3cf99d4f156b50785f376d37b7737
Sol Harter <92319512+Sol455@users.noreply.github.com>
2023-04-24 12:51:45 +0100

    Revert "circular buffer read"
    This reverts commit 555e265e5d1c7976a9b834c89d6acfb4bc98b909.

--- firmware/f7/build/events.d
+++
@@ -1,2 +0,0 @@
-build/events.o: Core/Src/events.c Core/Inc/events.h
-Core/Inc/events.h:
--- firmware/f7/build/mutff_core.d
+++
@@ -1,8 +0,0 @@
-build/mutff_core.o: mutff/src/mutff_core.c mutff/include/mutff_core.h \
- mutff/include/mutff_error.h mutff/include/mutff_io.h \
- mutff/include/mutff_error.h mutff/include/mutff_io.h
-mutff/include/mutff_core.h:
-mutff/include/mutff_error.h:
-mutff/include/mutff_io.h:
-mutff/include/mutff_error.h:
-mutff/include/mutff_io.h:
